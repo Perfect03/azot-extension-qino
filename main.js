4966f5f4c4cec39c2c285cafb7ffd465413b05a0
Perfect03 <zlobin_gg52926@mail.ru>
2026-09-22 23:19:32 +0500

    1.0.8

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {Lfstrm} from 'platform24tv-api'
+import {P24} from 'platform24tv-api'
 
-const QINO = new Lfstrm({DOMAIN_API: 'tele2', DOMAIN_FRONT: 'qino.live'})
+const QINO = new P24({DOMAIN_API: 'tele2', DOMAIN_FRONT: 'qino.live'})
 export default QINO.azotExtension
